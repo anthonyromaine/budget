97be75b62c53d5a57de351f4efa9bdec2e33e57e
Anthony Romaine <anthonyromaine@git>
2025-01-31 06:15:44 +0000

    Add functionality to read googlesheet file

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand';
+import axios from 'axios';
+import * as xlsx from 'xlsx';
 
 type Category = {
     name: string,
@@ -10,11 +12,23 @@ type Category = {
 interface BudgetState {
     budgetUrl: string | null,
     categories: Category[],
-    setBudgetUrl: (budgetUrl: string) => void
+    setBudgetUrl: (budgetUrl: string) => Promise<void>
 }
 
-export const useBudgetStore = create<BudgetState>((set) => ({
+export const useBudgetStore = create<BudgetState>((set, get) => ({
     budgetUrl: null,
     categories: [],
-    setBudgetUrl: (budgetUrl: string) => set({ budgetUrl }),
+    setBudgetUrl: async (budgetUrl: string) => {
+        let budgetData = (await axios.get(budgetUrl, {
+            responseType: "arraybuffer"
+        })).data;
+        let workbook = xlsx.read(budgetData, {
+            type: 'array'
+        })
+
+        for (let sheetName of workbook.SheetNames){
+            console.log(workbook.Sheets[sheetName])
+            console.log(xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]))
+        }
+    },
 }))
