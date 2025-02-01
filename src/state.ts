2f453ce9965c4443c41fc5da00f08899416f0b8b
Anthony Romaine <anthonyromaine@git>
2025-02-01 02:48:25 +0000

    Refactor functionality where budget data is converted

--- src/state.ts
+++ src/state.ts
@@ -19,19 +19,7 @@ type BudgetCSV = {
     Needs: string
 }
 
-type Budget = {
-    Date: Date | undefined,
-    Budget: number | undefined,
-    Categories: { name: string | undefined, amount: number | undefined }[]
-}
-
 type BudgetUrls = {budget: string, transactions: string, categories: string};
-
-interface BudgetState {
-    budgetUrl: boolean,
-    categories: Category[],
-    setBudgetUrl: (urls: BudgetUrls) => Promise<void>
-}
 
 const ValidCategories = type({
     name: "string",
@@ -39,14 +27,24 @@ const ValidCategories = type({
 });
 
 const ValidBudget = type({
-    Date: "Date",
-    Budget: "number",
-    Categories: ValidCategories.array()
+    date: "Date",
+    budget: "number",
+    categories: ValidCategories.array()
 });
+
+type Budget = typeof ValidBudget.infer;
+
+interface BudgetState {
+    budgetUrl: boolean,
+    categories: Category[],
+    budget: Budget[],
+    setBudgetUrl: (urls: BudgetUrls) => Promise<void>
+}
 
 export const useBudgetStore = create<BudgetState>((set, get) => ({
     budgetUrl: false,
     categories: [],
+    budget: [],
     setBudgetUrl: async (urls: BudgetUrls) => {
         // fetch data
         let budgetData = (await axios.get(urls.budget)).data;
@@ -66,29 +64,33 @@ export const useBudgetStore = create<BudgetState>((set, get) => ({
             header: true
         });
 
+        console.log(budgetCSVData)
+
         // convert data
-        let budget = budgetCSVData.map((monthlyBudget) => {
+        let budget: Budget[] = budgetCSVData.map((monthlyBudget) => {
             let entries = Object.entries(monthlyBudget);
-            let newMonthlyBudget: Budget = { Categories: [], Date: undefined, Budget: undefined };
+            let date, budget, categories = [];
             
             for (let [key, value] of entries){
                 if (key == "Date"){
-                    newMonthlyBudget.Date = monthlyBudget.Date.trim() ? new Date(monthlyBudget.Date.trim()) : undefined;
+                    date = monthlyBudget.Date.trim() ? new Date(monthlyBudget.Date.trim()) : undefined;
                 } else if(key == "Budget"){
-                    newMonthlyBudget.Budget = monthlyBudget.Budget.trim() ? Number(monthlyBudget.Budget.trim()): undefined;
+                    budget = monthlyBudget.Budget.trim() ? Number(monthlyBudget.Budget.trim()): undefined;
                 } else {
                     // everything else should be a category
-                    newMonthlyBudget.Categories?.push({ name: key.trim(), amount: value.trim() ? Number(value.trim()) : undefined })
+                    categories.push({ name: key.trim(), amount: value.trim() ? Number(value.trim()) : undefined })
                 }
             }
 
-            return newMonthlyBudget;
-        });
+            let newBudget = ValidBudget({ budget, date, categories });
 
-        budget = budget.filter(monthlyBudget => !(ValidBudget(monthlyBudget) instanceof type.errors));
+            if (!(newBudget instanceof type.errors)){
+                return newBudget;
+            }
+        }).filter(monthlyBudget => monthlyBudget);
 
         localStorage.setItem("urls", JSON.stringify(urls));
         console.log(budgetData, transactionData, categoriesData);
-        set({ budgetUrl: true });
+        set({ budgetUrl: true, budget });
     },
 }))
